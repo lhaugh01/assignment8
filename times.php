bdb80b95f74a84c858190c04128b392512a77c5b
lhaugh01 <lance_haugh@yahoo.com>
2025-03-28 18:22:35 -0400

    Update times.php

--- times.php
+++ times.php
@@ -1,1 +1,18 @@
+<?php
+// Get the number from the query string
+$n = isset($_GET['n']) ? intval($_GET['n']) : 1;
 
+// Validate the number
+if ($n < 1) {
+    $n = 1;
+}
+
+// Generate the times table
+echo "<h1>Times Table for $n</h1>";
+echo "<table border='1'>";
+for ($i = 1; $i <= 15; $i++) {
+    $result = $i * $n;
+    echo "<tr><td>$i x $n = $result</td></tr>";
+}
+echo "</table>";
+?>
